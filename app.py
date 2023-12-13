819b2e90ec6c2ffdb856278f1bc879821f1effbc
minjyun3011 <grand3canyon10@gmail.com>
2023-12-13 16:10:05 +0900

    Flaskアプリ完了

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#     if request.method == "GET":
+#         return render_template("index.html")
+#     else:
+#         message = request.form["message"]
+#         return render_template("index.html", message=message)
+
+@app.get("/")
+def index_get():
+    return render_template("index.html")
+
+@app.post("/")
+def index_post():
+    message = request.form["message"]
+    return render_template("index.html", message=message)
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
